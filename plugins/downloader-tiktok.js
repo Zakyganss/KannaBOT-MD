61dd96cf99d9da9ff9cdd5c9c7a4cf28b45f4d38
Zakyganss <107975463+Zakyganss@users.noreply.github.com>
2022-07-01 13:48:35 +0700

    Add files via upload

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,28 +1,54 @@
-import { tiktokdl } from '@bochilteam/scraper'
-
+/*const { tiktokdl, tiktokdlv2 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const { author: { nickname }, video, description } = await tiktokdl(args[0])
-    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
+    const url = video.no_watermark || video.no_watermark_hd || video.with_watermark || video.no_watermark_raw
     if (!url) throw 'Can\'t download video!'
-   let caption = `                   *「 T I K T O K 」*
-                 ████████▀▀▀████
-                 ████████────▀██
-                 ████████──█▄──█
-                 ███▀▀▀██──█████
-                 █▀──▄▄██──█████
-                 █──█████──█████
-                 █▄──▀▀▀──▄█████
-                 ███▄▄▄▄▄███████
-──────────────────────────
-*📛 Title:* ${data.title}
-*🖼️ Quality:* ${data.medias[1].quality}
-*📊 Size:* ${data.medias[1].forrmattedSize}`
-    await conn.sendHydrated(m.chat, caption, global.wm, data.medias[1].url, `${args[0]}`, 'Done😊', null, null, [[null, null],[null,null],[null,null]], m) 
-    })
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `
+🔗 *Url:* ${url}
+🧏 *Nickname:* ${nickname}${description ? `🖹 *Description:* ${description}` : ''}
+`.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tik(tok)?)$/i
 
-export default handler
+handler.command = /^(tik(tok)?(dl)?)$/i
+
+module.exports = handler*/
+
+
+const hxz = require("hxz-api")
+let handler = async(m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `*Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
+if (!args[0].match(/tiktok/gi)) throw `*Link salah! Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
+let p = await  hxz.ttdownloader(args[0])
+const { nowm, wm, audio } = p
+// made by aine
+ conn.sendFile(m.chat, nowm, 'tiktok.mp4', `*${global.wm}*`, m)
+}
+handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tiktok|tiktokdl)$/i
+handler.limit = true
+handler.group = true
+module.exports = handler
+
+/*
+const { tiktokdl, tiktokdlv2 } = require('@bochilteam/scraper')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `*Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
+    if (!args[0].match(/tiktok/gi)) throw `*Link salah! Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
+    const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
+    const url = video.no_watermark || video.no_watermark_hd || video.with_watermark || video.no_watermark_raw
+    if (!url) throw 'Can\'t download video!'
+    m.reply('Sedang diproses...')
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `*© Aine*
+`.trim(), m)
+}
+handler.help = ['tiktok <url>']
+handler.tags = ['downloader']
+
+handler.command = /^(tik|tt|tiktok)$/i
+
+module.exports = handler
+*/
